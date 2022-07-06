8f4382b70599d27b24abe06b7d9e32946ace3e5e
Cooper <cooperxmiller@gmail.com>
2022-07-05 22:37:03 -0500

    simplified how table data is being retrieved

--- dags/get_vinyl_records.py
+++ dags/get_vinyl_records.py
@@ -6,35 +6,42 @@ import json
 
 inventory = []
 
+
+@dataclass
+class Record:
+    artist: str
+    title: str
+    label: str
+    year: str
+
+
+@dataclass
+class Inventory:
+    records: list[Record]
+
+
+inventory = Inventory(records=[])
+
+
 def get_raw_page_data():
     artist = "119485-Tatsuro-Yamashita"
     url = f"https://www.discogs.com/artist/{artist}"
     page = requests.get(url)
-    soup = BeautifulSoup(page.content,'html.parser')
+    soup = BeautifulSoup(page.content, "html.parser")
     return soup
 
+
 soup = get_raw_page_data()
-table_rows = soup.find("table", class_="cards").findAll('tr', class_='card')
+table_rows = soup.find("table", class_="cards").findAll("tr", class_="card")
 
 for row in table_rows:
-    data = {
-        "artist": "",
-        "title": "",
-        "label": ""
-    }
-    
-    for td in row.findAll('td'):
-        td_class = td["class"]
-        
-        if "title" in td_class:
-            data["title"] = td.a.text
-        if "artist" in td_class:
-            # print(td.a.text)
-            data["artist"] = td.a.text
-        if "label" in td_class:
-            # print(td.a.text)
-            data["label"] = td.a.text
-    
-    inventory.append(data)
+    tds = row.findAll("td")
+    inventory.records.append(
+        Record(
+            artist=tds[3].text, title=tds[4].a.text, label=tds[5].text, year=tds[8].text
+        )
+    )
 
-print(inventory)
+
+print(len(inventory.records))
+print(inventory.records)
