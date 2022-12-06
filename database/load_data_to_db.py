ec813c832b9df244a3e058e55852e82027c4e8c3
Cooper <cooperxmiller@gmail.com>
2022-12-05 20:43:57 -0600

    fix: remove cursorclass

--- database/load_data_to_db.py
+++ database/load_data_to_db.py
@@ -29,8 +29,7 @@ def write_data_to_mysql(records: list[set]) -> None:
         user="root",
         passwd="root",
         db="vinyl_records",
-        port=3306,
-        cursorclass=MySQLdb.cursors.DictCursor,
+        port=3306
     )
     cursor = db.cursor()
     try:
@@ -52,7 +51,7 @@ def main():
     record_data = get_record_data_from_csv(FILE_NAME)
     clean_data = clean_record_data(record_data)
     write_data_to_mysql(clean_data)
-    print(f"Completed uploading {len(clean_data)} records to the database")
+    print(f"Uploaded {len(clean_data)} records to target database")
 
 
 if __name__ == "__main__":
